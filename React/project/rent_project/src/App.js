b299f26daf420a1107766558ca35db5b2e18d1e6
Vik1ang <vik1ang@outlook.com>
2021-10-29 15:00:39 +0800

    adding lib

--- React/project/rent_project/src/App.js
+++ React/project/rent_project/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import {Button} from "antd-mobile";
+
 function App() {
     return (
         <div className="App">
             项目根组件
+            <Button>登录</Button>
         </div>
     );
 }
