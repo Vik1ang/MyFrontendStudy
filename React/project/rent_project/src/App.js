71d101757dc80d1f58d38671c21ae989e0a7b701
Vik1ang <vik1ang@outlook.com>
2021-10-29 19:39:19 +0800

    balabala

--- React/project/rent_project/src/App.js
+++ React/project/rent_project/src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
 import {Button} from "antd-mobile";
+import CityList from "./pages/CityList";
+import Home from "./pages/Home"
 
 function App() {
     return (
-        <div className="App">
-            项目根组件
-            <Button>登录</Button>
-        </div>
+        <Router>
+            <div className="App">
+                <ul>
+                    <li><Link to="/home">首页</Link></li>
+                    <li><Link to="/citylist">城市选择</Link></li>
+                </ul>
+                <Route path="/home" component={Home}/>
+                <Route path="/citylist" component={CityList}/>
+            </div>
+        </Router>
+
     );
 }
 
