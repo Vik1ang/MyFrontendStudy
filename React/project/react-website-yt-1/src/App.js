59cc758c30bca531031c66991bb3685cf8a12ffe
Vik1ang <vik1ang@outlook.com>
2021-10-31 01:33:06 +0800

    commit

--- React/project/react-website-yt-1/src/App.js
+++ React/project/react-website-yt-1/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Services from "./pages/Services";
+import Products from "./pages/Products";
+import SignUp from "./pages/SignUp";
 
 function App() {
     return (
@@ -8,7 +12,10 @@ function App() {
             <Router>
                 <Navbar/>
                 <Switch>
-                    <Route path='/' exact/>
+                    <Route path='/' exact component={Home}/>
+                    <Route path='/services' component={Services}/>
+                    <Route path='/products' component={Products}/>
+                    <Route path='/sign-up' component={SignUp}/>
                 </Switch>
             </Router>
 
---
+++ React/project/react-website-yt-1/src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../../App.css';
+import HeroSection from '../../components/HereSection';
+import Cards from "../../components/Cards";
+import Footer from "../../components/Footer";
+
+export default () => {
+    return (
+        <>
+            <HeroSection/>
+            <Cards/>
+            <Footer/>
+        </>
+    )
+}
